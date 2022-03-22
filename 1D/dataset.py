88c018560df8d4a307272bef8e2944bb64e539bd
Yaniv Khaslavsky <45836033+ykhas@users.noreply.github.com>
2022-03-22 14:16:13 -0400

    Added test for sinusoidal boundary values

--- 1D/dataset.py
+++ 1D/dataset.py
@@ -1,17 +1,31 @@
+from tokenize import Number
 import torch
 from torch.utils.data import Dataset
+from dataclasses import dataclass
 
 class WaveDataset(Dataset):
-  def __init__(self, w, dataset_size=1000, full_res_multiplier=4):
-    self.dataset_size = dataset_size
-    self.w = w
+  def __init__(self, width, num_hidden_state_dataset_size=1000):
+    self.num_hidden_state_dataset_size = num_hidden_state_dataset_size
+    self.width = width
 
-    self.boolean_masks = torch.zeros(dataset_size, w)
-    self.boundary_values = torch.zeros(dataset_size, w)
-    self.hidden_states = torch.zeros(dataset_size, w-1)
+    self.boolean_masks = torch.zeros(num_hidden_state_dataset_size, 1, width)
+    self.boundary_values = torch.zeros(num_hidden_state_dataset_size, 1, width)
+    self.hidden_states = torch.zeros(num_hidden_state_dataset_size, width-1)
+    self.phases = torch.randn(self.num_hidden_state_dataset_size)
+    self.__initialize_boundaries()
+
+  def __initialize_boundaries(self):
+    '''
+    Initializes random sinusoidal boundary conditions
+    '''
+    self.boolean_masks[:, 0, -1: 2] = 1 # set domain boundary
+    emitter_size = 2
+    sinusoids = torch.sin(self.phases.unsqueeze(1)).expand(-1, emitter_size) # create random sinusoids as the emitter's state
+    self.boolean_masks[:, 0, self.width//2: self.width // 2 + emitter_size] = 1 # emitter boundary
+    self.boundary_values[:, 0, self.width//2: self.width // 2 + emitter_size] = sinusoids
 
   def __getitem__(self, idx):
-    return self.boundary_values[idx], self.boolean_masks[idx], self.hidden_states[idx]
+    return self.boolean_masks[idx], self.boundary_values[idx], self.hidden_states[idx]
 
   def update_items(self, idx, boolean_mask, boundary_value, hidden_state):
     self.boolean_masks[idx] = boolean_mask 
@@ -19,5 +33,5 @@ class WaveDataset(Dataset):
     self.hidden_states[idx] = hidden_state
 
   def __len__(self):
-    return self.dataset_size
+    return self.num_hidden_state_dataset_size
 
--- 1D/test.py
+++ 1D/test.py
@@ -1,29 +1,87 @@
 from dataset import WaveDataset
+from wave_train_1D import WaveModel, Loss_Calculator, train
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch
 import copy
 import torch
 import unittest
 
+domain_width = 200
+
+
 class TestDataSet(unittest.TestCase):
-  def test_dataset_change(self):
-    domain_width = 200
-    dataset = WaveDataset(domain_width)
-    dataloader = DataLoader(dataset, 100)
+    def test_dataset_change(self):
+        dataset = WaveDataset(domain_width)
+        dataloader = DataLoader(dataset, 100)
 
-    data_iterable = iter(dataloader)
+        data_iterable = iter(dataloader)
 
-    first_items_copy = copy.deepcopy(dataset.__getitem__(0))
+        first_items_copy = copy.deepcopy(dataset.__getitem__(0))
 
-    fake_item = torch.ones(domain_width)
-    fake_state = torch.ones(domain_width - 1)
-    dataset.update_items(0, fake_item,fake_item,fake_state)
+        fake_item = torch.ones(domain_width)
+        fake_state = torch.ones(domain_width - 1)
+        dataset.update_items(0, fake_item, fake_item, fake_state)
 
-    first_items_real = next(data_iterable)
+        first_items_real = next(data_iterable)
 
-    for real_item, copy_item in zip(first_items_copy, first_items_real):
-      self.assertFalse(torch.equal(real_item, copy_item))
+        for real_item, copy_item in zip(first_items_copy, first_items_real):
+            self.assertFalse(torch.equal(real_item, copy_item))
 
-  
-if __name__ == "main":
-  unittest.main()
+    def test_dataset_phase(self):
+        torch.manual_seed(0)
+        expected_boundary_values = torch.tensor([[[0.000000000,  0.000000000,  0.000000000,  0.000000000,  0.000000000,
+                                                   0.999556005,  0.999556005,  0.000000000,  0.000000000,  0.000000000]],
 
+                                                 [[0.000000000,  0.000000000,  0.000000000,  0.000000000,  0.000000000,
+                                                   -0.289236248, -0.289236248,  0.000000000,  0.000000000,  0.000000000]],
+
+                                                 [[0.000000000,  0.000000000,  0.000000000,  0.000000000,  0.000000000,
+                                                   -0.820796072, -0.820796072,  0.000000000,  0.000000000,  0.000000000]],
+
+                                                 [[0.000000000,  0.000000000,  0.000000000,  0.000000000,  0.000000000,
+                                                   0.538310647,  0.538310647,  0.000000000,  0.000000000,  0.000000000]],
+
+                                                 [[0.000000000,  0.000000000,  0.000000000,  0.000000000,  0.000000000,
+                                                   -0.884080291, -0.884080291,  0.000000000,  0.000000000,  0.000000000]]])
+
+        dataset = WaveDataset(10, num_hidden_state_dataset_size=5)
+        self.assertTrue(torch.equal(
+            dataset.boundary_values, expected_boundary_values))
+
+
+class TestUtils:
+    @staticmethod
+    def generate_sample_state():
+        model = WaveModel(2, 2)
+        # 1s around border, zero in the middle.
+        z_boundary_cond = F.pad(torch.zeros(
+            1, 1, domain_width - 4), (2, 2), value=1)
+        # emitter is in the middle, zeros elsewhere
+        z_emitter_mask = F.pad(torch.ones(
+            1, 1, 4), (domain_width // 2 - 2, domain_width // 2 - 2), value=0)
+        hidden_state = torch.randn(1, 5, domain_width - 1)
+        return model, z_boundary_cond, z_emitter_mask, hidden_state
+
+
+class TestForwardModel(unittest.TestCase):
+    def test_forward_computation(self):
+        model, z_boundary_cond, z_emitter_mask, hidden_state = TestUtils.generate_sample_state()
+        model.forward(hidden_state, z_boundary_cond, z_emitter_mask)
+
+# class TestTrainModel(unittest.TestCase):
+    # def test_train_iteration(self):
+    #   model, z_boundary_cond, z_emitter_mask, hidden_state = TestUtils.generate_sample_state()
+    #   dataset = WaveDataset(domain_width)
+    #   loss_calc = Loss_Calculator(0.1, 0.5)
+    #   train(dataset, epochs = 1, n_batches = 1, n_samples = 1, loss_calc=loss_calc)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+    # to run with debugger, comment above line and uncomment the two below.
+    # model_train_test = TestTrainModel()
+    # model_train_test.test_train_iteration()
+    # dataset_test = TestDataSet()
+    # dataset_test.test_dataset_phase()
